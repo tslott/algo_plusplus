21235269ef66df37ddea7aa03b2f249b34c2b7e2
Tobias Slott Jensen <tsj@damvad.com>
2020-04-26 00:40:49 +0200

    implemented insert document functionality in inverted index

--- search_index/inverted_index.cpp
+++ search_index/inverted_index.cpp
@@ -3,16 +3,53 @@
 inverted_index::inverted_index(string documents_folder){
     // Build index from all documents
     // Use multithreading
+    std::cout << documents_folder << "\n";
 }
 
 inverted_index::~inverted_index(){}
 
 void inverted_index::insert_document(string file_name)
 {
-    // string doc_id, doc_content;
+    // Extract document id from file name
+    string doc_id = file_name.substr(0, file_name.size()-4);
+    doc_id = doc_id.substr(9, doc_id.size());
+
+    // Open txt file and stream words in document one by one, document is split on whitespace.
+    std::ifstream file;
+    file.open(file_name);
+    string word;
+    while (file >> word)
+    {
+        // Clean word for all non-alpha characters
+        word.erase(
+            std::remove_if(
+                word.begin(),
+                word.end(),
+                [](unsigned char c) { return !std::isalpha(c); }
+            ),
+            word.end()
+        );
+
+        // Convert string to lowercase
+        std::transform(word.begin(), word.end(), word.begin(), ::tolower);
+
+        // Insert word into inverted index
+        index[word][doc_id].push_back(0);
+    }
 }
 
 void search(string search_word)
 {
 
+}
+
+void inverted_index::print()
+{
+    for (const auto &[word, doc_map] : index)
+    {
+        std::cout << word << "\n";
+        for (const auto &[doc_id, v] : doc_map)
+            std::cout << "\t" << doc_id << "\n";
+        std::cout << "\n";
+    }
 }
--- search_index/inverted_index.h
+++ search_index/inverted_index.h
@@ -1,7 +1,11 @@
 #ifndef IVERTED_INDEX_GUARD
 #define IVERTED_INDEX_GUARD
+#include <iostream>
 #include <string>
 #include <map>
+#include <fstream>
+#include <algorithm>
+#include <vector>
 
 using std::string;
 using std::map;
@@ -9,12 +13,13 @@ using std::map;
 class inverted_index
 {
 private:
-    map<string,map<string,int> > index;
+    map<string, map<string, std::vector<int> > > index;
 public:
     inverted_index(string documents_folder);
     ~inverted_index();
     void insert_document(string file_name);
     void search(string search_word);
+    void print();
 };
 
 #endif
